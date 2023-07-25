812617d47f0aa11a29bfa5c2450747aa8cb8662f
SimoneVilardo <simovila95@gmail.com>
2023-07-25 14:43:29 +0200

    sistemati collegamenti tra le pagine e il database

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Comics;
 
 class PageController extends Controller
 {
-    //
+    public function index(){
+        $comics = Comics::all();
+        dd($comics);
+        return view('home', compact('comics'));
+    }
 }
